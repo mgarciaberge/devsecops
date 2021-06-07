e176c28c75cd4be9a89f548aedd0149f8de9c1c7
Oscar Castillo <del.castillo.oscar.a@gmail.com>
2021-06-07 19:55:18 +0200

    Test precomit

--- devsecops/settings.py
+++ devsecops/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b7u2=@ds3#!n2%+zvnedn1+5g!cyi+a$b4d7v&&7p)8ym+kn3v'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
